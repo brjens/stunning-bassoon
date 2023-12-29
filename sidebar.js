79c29836064b7f4d7b115b78b1f679affabc9fe3
yoga <brjensen121@gmail.com>
2023-12-29 14:33:38 -0500

    changed sidebar from hardcode to progromatically

---
+++ sidebar.js
@@ -0,0 +1,7 @@
+// sidebar.js
+  const sidebar = document.getElementById('sidebar');
+  sidebar.innerHTML = '<a href="index.html">Home</a>' + 
+	'<a href="schoolProjects.html">School Projects</a>' +
+	'<a href="contact.html" >Contact</a>' +
+	'<a href="https://www.linkedin.com/in/brandon-jensen-6bb8b688/" target="blank">LinkedIn</a>' +	
+        '<a href="https://github.com/brjens" target="blank">GitHub</a>';
